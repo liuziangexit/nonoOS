295cfce48671157a089a0c867ab82a42efa1588a
liuziangexit <liuziang@liuziangexit.com>
2020-07-26 18:13:56 +0800

    我代表党中央，国务院，中央军委，武警总队，向你致以崇高的敬意！

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 set -e
-./build.sh
-qemu-system-i386 -d int -no-reboot -no-shutdown -cdrom nonoOS.iso
-#qemu-system-i386 -cdrom nonoOS.iso
+make
+tools/mkfs fs.img
+
+CPU=1
+MEM=256
+
+qemu-system-i386 -d int -no-reboot -no-shutdown -serial mon:stdio\
+  -drive file=fs.img,index=1,media=disk,format=raw -drive file=nonoOS.img,index=0,media=disk,format=raw -smp $CPU -m $MEM
