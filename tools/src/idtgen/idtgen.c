f54d3e37d41bd92a59f361dcb6a479ddf7637e42
liuziangexit <liuziang@liuziangexit.com>
2020-08-23 13:47:06 +0800

    no message

---
+++ tools/src/idtgen/idtgen.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(int argc, char **args) {
+  if (argc != 2) {
+    printf("Usage: idtgen filename\n");
+    return 0;
+  }
+  FILE *fp = fopen(args[1], "w+");
+  if (fp == NULL) {
+    printf("could not write to %s\n", args[1]);
+    return 0;
+  }
+
+  fprintf(fp, "# handler\n");
+  fprintf(fp, ".text\n");
+  fprintf(fp, ".globl __alltraps\n");
+
+  int i;
+  for (i = 0; i < 256; i++) {
+    fprintf(fp, ".globl vector%d\n", i);
+    fprintf(fp, "vector%d:\n", i);
+    if ((i < 8 || i > 14) && i != 17) {
+      fprintf(fp, "  pushl $0\n");
+    }
+    fprintf(fp, "  pushl $%d\n", i);
+    fprintf(fp, "  jmp __alltraps\n");
+  }
+  fprintf(fp, "\n");
+  fprintf(fp, "# vector table\n");
+  fprintf(fp, ".data\n");
+  fprintf(fp, ".globl __vectors\n");
+  fprintf(fp, "__vectors:\n");
+  for (i = 0; i < 256; i++) {
+    fprintf(fp, "  .long vector%d\n", i);
+  }
+
+  fclose(fp);
+  return 0;
+}
