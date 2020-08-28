2ae6589c10bf185545329a27dcec38e88bc8be2f
liuziangexit <liuziang@liuziangexit.com>
2020-08-28 20:36:09 +0800

    抄抄改改printf

--- libno/include/stdlib.h
+++ libno/include/stdlib.h
@@ -8,6 +8,9 @@ extern "C" {
 void abort(void);
 void exit(int);
 
+//将数字转为其字符串表示
+int itoa(int num, unsigned char *str, int len, int base);
+
 #ifdef __cplusplus
 }
 #endif
--- libno/src/stdout.c
+++ libno/src/stdout.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <limits.h>
 #include <stdarg.h>
 #include <stdbool.h>
@@ -11,14 +12,11 @@ int putchar(int ic) {
   return ic;
 }
 
-static bool print(const char *data, size_t length) {
-  const unsigned char *bytes = (const unsigned char *)data;
-  for (size_t i = 0; i < length; i++)
-    if (putchar(bytes[i]) == EOF)
-      return false;
-  return true;
+static void print(const char *data, size_t length) {
+  terminal_write(data, length);
 }
 
+// TODO 瞎xx写的printf，以后重构掉或者抄一个来
 int printf(const char *restrict format, ...) {
   va_list parameters;
   va_start(parameters, format);
@@ -38,8 +36,7 @@ int printf(const char *restrict format, ...) {
         // TODO: Set errno to EOVERFLOW.
         return -1;
       }
-      if (!print(format, amount))
-        return -1;
+      print(format, amount);
       format += amount;
       written += amount;
       continue;
@@ -54,8 +51,7 @@ int printf(const char *restrict format, ...) {
         // TODO: Set errno to EOVERFLOW.
         return -1;
       }
-      if (!print(&c, sizeof(c)))
-        return -1;
+      print(&c, sizeof(c));
       written++;
     } else if (*format == 's') {
       format++;
@@ -65,8 +61,47 @@ int printf(const char *restrict format, ...) {
         // TODO: Set errno to EOVERFLOW.
         return -1;
       }
-      if (!print(str, len))
+      print(str, len);
+      written += len;
+    } else if (*format == 'd') {
+      format++;
+      int v = va_arg(parameters, int);
+      if (!maxrem) {
+        // TODO: Set errno to EOVERFLOW.
         return -1;
+      }
+      static char sv[10];
+      if (itoa(v, sv, 10, 10))
+        return -1; // itoa failed
+      size_t len = strlen(sv);
+      print(sv, len);
+      written += len;
+    } else if (strlen(format) > 3 && isdigit(*format) &&
+               isdigit(*(format + 1)) && *(format + 2) == 'x') {
+      char placeholder = *format;
+      format++;
+      int digit_cnt = *format - '0';
+      format++;
+      format++; // skip x
+      int v = va_arg(parameters, int);
+      if (!maxrem) {
+        // TODO: Set errno to EOVERFLOW.
+        return -1;
+      }
+      static char sv[11];
+      if (itoa(v, sv, 11, 16))
+        return -1; // itoa failed
+      size_t len = strlen(sv);
+      if (len < digit_cnt) {
+        int ph_add = digit_cnt - len;
+        if (len + ph_add <= sizeof(sv) - 1) {
+          memcpy(sv + ph_add, sv, len);
+          memset(sv, placeholder, ph_add);
+          len += ph_add;
+          sv[len] = 0;
+        }
+      }
+      print(sv, len);
       written += len;
     } else {
       format = format_begun_at;
@@ -75,8 +110,7 @@ int printf(const char *restrict format, ...) {
         // TODO: Set errno to EOVERFLOW.
         return -1;
       }
-      if (!print(format, len))
-        return -1;
+      print(format, len);
       written += len;
       format += len;
     }
