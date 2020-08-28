3c3ac8b07e21ae977fac3ccf11bf481dee678a64
liuziangexit <liuziang@liuziangexit.com>
2020-08-28 14:09:31 +0800

    好吧，原来loader有义务把bss清0。我以前以为不用，但是后来static变量没有被初始化成0，某些逻辑就翻车了。
    原来gcc对于local static变量的初始化有这么一个逻辑：如果初始化为非0，那么由gcc插入初始化的代码；如果初始化为0，那么gcc啥也不做。
    为啥gcc啥也不做呢，就是因为gcc假定loader已经把local static variable所在的bss段全部清0了！所以他自己就不再清一次了
    
    去吃饭

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -13,6 +13,19 @@
 #include <x86.h>
 
 void kentry(void) {
+  /*
+  实际上在编译器与loader之间有一个约定，就是loader要负责把bss段全部清0
+  之前没有把bss清0，所以程序里的static变量没有被初始化，于是就产生了逻辑错误
+  https://en.wikipedia.org/wiki/.bss#BSS_in_C
+
+  另外，这里extern char[]也有讲究，不能写成extern char*
+  如果写成了extern char*，那这些值会变成0xffff这样的东西，
+  然后接下来memset往0xffff地址写的时候就会挂掉。
+  这个我也想不通是怎么回事，因为我们都知道char[]和char*实际上是一个东西（虽然在语言层面不是）
+  */
+  extern char bss_begin[], bss_end[];
+  memset(bss_begin, 0, bss_end - bss_begin);
+
   gdt_init();
   pic_init();
   idt_init();
