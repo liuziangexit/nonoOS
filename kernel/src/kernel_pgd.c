05ba118adbb2d823cbdf1f4474055d08096f0687
liuziangexit <liuziang@liuziangexit.com>
2020-08-29 20:02:39 +0800

    好了

--- kernel/src/kernel_pgd.c
+++ kernel/src/kernel_pgd.c
@@ -5,22 +5,25 @@
 // 页目和页表必须对齐到页边界
 // PTE_PS in a page directory entry enables 4Mbyte pages.
 
+// FIXME 现在暂时去掉ring限制，ring3就能访问全部页面
+
 _Alignas(PGSIZE) int32_t kernel_page_directory[NPDENTRIES] = {
     // Map Virtual's [0, 4MB) to Physical's [0, 4MB)
-    [0] = 0x0 | PTE_P | PTE_W | PTE_PS,
+    [0] = 0x0 | PTE_P | PTE_W | PTE_PS | PTE_U,
 
     // 映射内核地址开始的8MB空间
     // Map Virtual's [KERNBASE, KERNBASE + 4MB) to Physical's [0, 4MB)
-    [KERNEL_VIRTUAL_BASE >> PDXSHIFT] = 0x0 | PTE_P | PTE_W | PTE_PS,
+    [KERNEL_VIRTUAL_BASE >> PDXSHIFT] = 0x0 | PTE_P | PTE_W | PTE_PS | PTE_U,
     // Map Virtual's [KERNBASE + 4MB, KERNBASE + 8MB) to Physical's [4MB, 8MB)
     [(KERNEL_VIRTUAL_BASE + 0x400000) >> PDXSHIFT] = 0x400000 | PTE_P | PTE_W |
-                                                     PTE_PS,
+                                                     PTE_PS | PTE_U,
     // 映射16KB的内核栈
     [(KERNEL_VIRTUAL_BASE + KERNEL_STACK) >> PDXSHIFT] = KERNEL_STACK | PTE_P |
-                                                         PTE_W | PTE_PS,
+                                                         PTE_W | PTE_PS | PTE_U,
     [(KERNEL_VIRTUAL_BASE + KERNEL_STACK + 0x400000) >>
-        PDXSHIFT] = (KERNEL_STACK + 0x400000) | PTE_P | PTE_W | PTE_PS,
+        PDXSHIFT] = (KERNEL_STACK + 0x400000) | PTE_P | PTE_W | PTE_PS | PTE_U,
     [(KERNEL_VIRTUAL_BASE + KERNEL_STACK + 0x800000) >>
-        PDXSHIFT] = (KERNEL_STACK + 0x800000) | PTE_P | PTE_W | PTE_PS,
+        PDXSHIFT] = (KERNEL_STACK + 0x800000) | PTE_P | PTE_W | PTE_PS | PTE_U,
     [(KERNEL_VIRTUAL_BASE + KERNEL_STACK + 0x1000000) >>
-        PDXSHIFT] = (KERNEL_STACK + 0x1000000) | PTE_P | PTE_W | PTE_PS};
+        PDXSHIFT] = (KERNEL_STACK + 0x1000000) | PTE_P | PTE_W | PTE_PS |
+                    PTE_U};
