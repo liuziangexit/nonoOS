1cbc5c975336db46615c8d3c7cd79b2de76dde44
liuziangexit <liuziang@liuziangexit.com>
2020-10-11 14:13:09 +0800

    消warning

--- program/hello_world/hello.c
+++ program/hello_world/hello.c
@@ -1,7 +1,10 @@
 //#include <stdio.h>
 //#include <task.h>
+#include <compiler_helper.h>
 
 int main(int argc, char **argv) {
+  UNUSED(argc);
+  UNUSED(argv);
   // printf("hello world from user process %d\n", task_current());
   return 0;
 }
