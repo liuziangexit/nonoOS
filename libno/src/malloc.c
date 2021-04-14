66596dc90974f556b1dff9f00315da64f137dcd4
liuziangexit <liuziang@liuziangexit.com>
2021-04-14 21:39:53 +0800

    实现C11标准的aligned_alloc

--- libno/src/malloc.c
+++ libno/src/malloc.c
@@ -3,4 +3,9 @@
 
 void *malloc(size_t size) { return kmem_cache_alloc(size); }
 
+void *aligned_alloc(size_t alignment, size_t size) {
+  assert(size % alignment == 0);
+  return malloc(size);
+}
+
 void free(void *p) { kmem_cache_free(p); }
