7445e66259b4011300d5d91eaf90141151b3a257
liuziangexit <liuziang@liuziangexit.com>
2021-04-24 17:34:10 +0800

    上年纪了，容易困

--- kernel/include/virtual_memory.h
+++ kernel/include/virtual_memory.h
@@ -1,6 +1,7 @@
 #ifndef __KERNEL_VIRTUAL_MEMORY_H__
 #define __KERNEL_VIRTUAL_MEMORY_H__
 #include <avlmini.h>
+#include <stdbool.h>
 
 // struct virtual_memory;
 
@@ -9,7 +10,7 @@ struct virtual_memory_area {
   struct avl_node avl_node;
   // struct virtual_memory *vm;
   uintptr_t vma_start;
-  uint32_t vma_size;
+  uint32_t vma_size; // in bytes
   // uint16_t vma_flag;
 };
 
@@ -18,5 +19,18 @@ struct virtual_memory {
   // uint32_t vma_cnt;
   uint32_t *page_directory;
 };
+
+//初始化一个虚拟地址空间结构
+struct virtual_memory *virtual_memory_create();
+//销毁一个虚拟地址空间结构
+void virtual_memory_destroy(struct virtual_memory *vm);
+//在一个虚拟地址空间结构中进行以4k为边界映射
+//返回false如果指定的虚拟地址已经有映射了
+bool virtual_memory_map(struct virtual_memory *vm, uintptr_t vma_start,
+                        uintptr_t vma_size);
+void virtual_memory_unmap(struct virtual_memory *vm, uintptr_t vma_start);
+//在一个虚拟地址空间结构中寻找空闲的指定长度的地址空间
+//返回0表示找不到
+uintptr_t virtual_memory_find(struct virtual_memory *vm, uint32_t vma_size);
 
 #endif
