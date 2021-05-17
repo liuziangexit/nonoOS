43f8c464c71d60f4cc4d884b968fe7ef8144e37d
liuziangexit <liuziang@liuziangexit.com>
2021-05-18 00:13:08 +0800

    no message

--- program/hello_world/hello.c
+++ program/hello_world/hello.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
   volatile uint32_t *mem2 = malloc(4);
   volatile uint32_t *mem3 = malloc(4);
   *mem2 = 999;
-  free((void *)mem);
+  // free((void *)mem);
   // free(mem);
   free((void *)mem2);
   free((void *)mem3);
