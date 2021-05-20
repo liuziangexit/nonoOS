8c5c348fbe899bfa9ee47c387c38911d1d245eb5
liuziangexit <liuziang@liuziangexit.com>
2021-05-20 12:02:20 +0800

    no message

--- kernel/include/clock.h
+++ kernel/include/clock.h
@@ -2,7 +2,8 @@
 #define __KERNEL_CLOCK_H__
 #include <stdlib.h>
 
-#define TICK_PER_SECOND 100
+#define TICK_TIME_MS 10
+#define TICK_PER_SECOND (1000 / TICK_TIME_MS)
 
 void clock_init();
 uint64_t clock_count_tick();
