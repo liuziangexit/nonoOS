847db1ebbc632b4e766777869de9de25b9e387b5
liuziangexit <liuziang@liuziangexit.com>
2021-05-20 15:01:33 +0800

    no message

--- tools/stats.sh
+++ tools/stats.sh
@@ -1,5 +1,9 @@
+printf 'stats of driver:\n'
 cloc ../driver
+printf '\nstats of kernel:\n'
 cloc ../kernel
+printf '\nstats of libno:\n'
 cloc ../libno
+printf '\nstats of program:\n'
 cloc ../program
 
