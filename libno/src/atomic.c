d72bd7b399baba5e2861f73ad3820849b66af843
liuziangexit <liuziang@liuziangexit.com>
2021-06-02 21:02:04 +0800

    no message

--- libno/src/atomic.c
+++ libno/src/atomic.c
@@ -3,12 +3,12 @@
 
 void atomic_store(uint32_t *dst, uint32_t val) {
   memory_barrier(RELEASE);
-  asm("movl %0, (%1)" : : "r"(val), "r"(dst) : "cc", "memory");
+  asm volatile("movl %0, (%1)" : : "r"(val), "r"(dst) : "cc", "memory");
 }
 
 uint32_t atomic_load(uint32_t *src) {
   uint32_t val;
-  asm("movl (%1), %0" : "=r"(val) : "r"(src) : "cc", "memory");
+  asm volatile("movl (%1), %0" : "=r"(val) : "r"(src) : "cc", "memory");
   memory_barrier(ACQUIRE);
   return val;
 }
@@ -16,7 +16,10 @@ uint32_t atomic_load(uint32_t *src) {
 uint32_t atomic_exchange(uint32_t *dst, uint32_t val) {
   uint32_t prev;
   memory_barrier(RELEASE);
-  asm("xchg %1, %0" : "=m"(*dst), "=a"(prev) : "a"(val) : "cc", "memory");
+  asm volatile("xchg %1, %0"
+               : "=m"(*dst), "=a"(prev)
+               : "a"(val)
+               : "cc", "memory");
   memory_barrier(ACQUIRE);
   return prev;
 }
@@ -25,31 +28,37 @@ bool atomic_compare_exchange(uint32_t *dst, uint32_t *expected,
                              uint32_t desired) {
   uint32_t prev = *expected;
   memory_barrier(RELEASE);
-  asm("lock; cmpxchg %2, %0"
-      : "=m"(*dst), "=a"(*expected)
-      : "r"(desired), "a"(*expected)
-      : "cc", "memory");
+  asm volatile("lock; cmpxchg %2, %0"
+               : "=m"(*dst), "=a"(*expected)
+               : "r"(desired), "a"(*expected)
+               : "cc", "memory");
   memory_barrier(ACQUIRE);
   return prev == *expected;
 }
 
 uint32_t atomic_add(uint32_t *dst, uint32_t add) {
   memory_barrier(RELEASE);
-  asm("lock; add %1, %0" : "=m"(*dst) : "r"(add), "m"(*dst) : "cc", "memory");
+  asm volatile("lock; add %1, %0"
+               : "=m"(*dst)
+               : "r"(add), "m"(*dst)
+               : "cc", "memory");
   memory_barrier(ACQUIRE);
   return *dst;
 }
 
 uint32_t atomic_sub(uint32_t *dst, uint32_t sub) {
   memory_barrier(RELEASE);
-  asm("lock; sub %1, %0" : "=m"(*dst) : "r"(sub), "m"(*dst) : "cc", "memory");
+  asm volatile("lock; sub %1, %0"
+               : "=m"(*dst)
+               : "r"(sub), "m"(*dst)
+               : "cc", "memory");
   memory_barrier(ACQUIRE);
   return *dst;
 }
 
 uint32_t atomic_fetch_add(uint32_t *dst, uint32_t add) {
   memory_barrier(RELEASE);
-  asm("lock; xaddl %0, %1" : "+r"(add), "+m"(*dst) : : "cc", "memory");
+  asm volatile("lock; xaddl %0, %1" : "+r"(add), "+m"(*dst) : : "cc", "memory");
   memory_barrier(ACQUIRE);
   return add;
 }
